a102258eef503a0bf32b79cfcc95442ddfa804f0
Iago Albuquerque <iagoalbuquerque@Iago-MacBook-Air.local>
2018-01-15 15:17:48 -0300

    remove cached files

--- DerivedData/Senuti/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/NVActivityIndicatorView.build/Objects-normal/x86_64/NVActivityIndicatorView_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/iagoalbuquerque/Desktop/development/Senuti/DerivedData/Senuti/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/NVActivityIndicatorView.build/DerivedSources/NVActivityIndicatorView_vers.c
